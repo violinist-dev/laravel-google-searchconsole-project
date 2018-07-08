b03bbc8002b3f382b260f7b0870c94612d6943fe
kawax <kawaxbiz@gmail.com>
2018-07-09 01:32:02 +0900

    positions

--- resources/views/home/site/positions.blade.php
+++ resources/views/home/site/positions.blade.php
@@ -1,16 +1,16 @@
 <td>{{ $positions[$id][2] }}</td>
 
-@if($positions[$id][2] > $positions[$id][1])
+@if($positions[$id][2] < $positions[$id][1])
     <td class="table-danger">{{ $positions[$id][1] }}</td>
-@elseif($positions[$id][2] < $positions[$id][1])
+@elseif($positions[$id][2] > $positions[$id][1])
     <td class="table-success">{{ $positions[$id][1] }}</td>
 @else
     <td>{{ $positions[$id][1] }}</td>
 @endif
 
-@if($positions[$id][1] > $positions[$id][0])
+@if($positions[$id][1] < $positions[$id][0])
     <td class="table-danger">{{ $positions[$id][0] }}</td>
-@elseif($positions[$id][1] < $positions[$id][0])
+@elseif($positions[$id][1] > $positions[$id][0])
     <td class="table-success">{{ $positions[$id][0] }}</td>
 @else
     <td>{{ $positions[$id][0] }}</td>
