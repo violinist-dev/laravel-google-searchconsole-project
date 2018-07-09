50afc4daa9b555109a21a6c115a1dbc3453aee6e
kawax <kawaxbiz@gmail.com>
2018-07-09 10:06:46 +0900

    analytics

--- resources/views/layouts/analytics.blade.php
+++ resources/views/layouts/analytics.blade.php
@@ -1,6 +1,6 @@
 @unless(empty(config('sc.analytics')))
 <!-- Global Site Tag (gtag.js) - Google Analytics -->
-<script async src="https://www.googletagmanager.com/gtag/js?id={{ config('hatena.analytics') }}"></script>
+<script async src="https://www.googletagmanager.com/gtag/js?id={{ config('sc.analytics') }}"></script>
 <script>
   window.dataLayer = window.dataLayer || [];
   function gtag(){dataLayer.push(arguments)};
